2bc0b241e20ccdc9e03007b63402c0237aeb11a6
Aron Dennen <aron@arondennen.com>
2014-11-30 17:52:03 -0500

    Initial version

---
+++ FizzBuzz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    char buf[3];
+
+    printf("A FizzBuzz implementation in C by Aron Dennen (c)2014\n\n");
+
+    for (i = 1; i <= 100; i++)
+        printf("%s\n", ( (i % 3) && (i % 5) ) ? snprintf(buf, 3, "%d", i), buf : !(i % 3) ?
+            !(i % 5) ? "FizzBuzz" : "Fizz" : "Buzz");
+
+    return 0;
+}
