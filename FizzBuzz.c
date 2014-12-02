9655fb8e9ff3f02492194aa33be8b0c390316c55
Aron Dennen <aron@arondennen.com>
2014-12-02 02:12:26 -0500

    Changed printf to puts for performance

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -4,10 +4,10 @@ int main() {
     int i;
     char buf[3];
 
-    printf("A FizzBuzz implementation in C by Aron Dennen (c)2014\n\n");
+    puts("A FizzBuzz implementation in C by Aron Dennen (c)2014\n\n");
 
     for (i = 1; i <= 100; i++)
-        printf("%s\n", ( (i % 3) && (i % 5) ) ? snprintf(buf, 3, "%d", i), buf : !(i % 3) ?
+        puts( ( (i % 3) && (i % 5) ) ? snprintf(buf, 3, "%d", i), buf : !(i % 3) ?
             !(i % 5) ? "FizzBuzz" : "Fizz" : "Buzz");
 
     return 0;
